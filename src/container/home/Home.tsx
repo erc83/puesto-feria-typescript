d601f69f23ed006026cad1cd9acce8c020823b19
erc83 <espinoza.eric@gmail.com>
2021-07-15 14:40:01 -0400

    Some render usando nombre

--- src/container/home/Home.tsx
+++ src/container/home/Home.tsx
@@ -7,17 +7,17 @@ import stock3 from "../../data/stockSucursal3.json"
 const Home = () => {
     return(
         <>
-        <div>
-            <h1 className={styles.titulo}>Bienvenido a mi puesto de Feria </h1>
-        </div>
-        <h4>Productos en stock sucursal 1</h4>
-        <Productos productos = {stock1}/>
+            <div>p
+                <h1 className={styles.titulo}>Bienvenido a mi puesto de Feria </h1>
+            </div>
+            <h4>Productos en stock sucursal 1</h4>
+            {stock1.some(({nombre})=>nombre==="frutilla")&&<Productos productos = {stock1}/>}
 
-        <h4>Productos en stock sucursal 2</h4>
-        <Productos productos = {stock2}/>
+            <h4>Productos en stock sucursal 2</h4>
+            {stock2.some(({nombre})=>nombre==="Uva 2")&&<Productos productos = {stock2}/>}
 
-        <h4>Productos en stock sucursal 3</h4>
-        <Productos productos = {stock3}/>
+            <h4>Productos en stock sucursal 3</h4>
+            <Productos productos = {stock3}/>
         </>
     )
 }
